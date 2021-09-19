005e010bf751e341a407e1026a00fb4e98d66646
Lopes <gus.lcampos38@gmail.com>
2021-09-19 18:31:13 -0300

    Updates

---
+++ precache-manifest.55bd5d3c41b3266b06189f68dc138313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad61ed49bef5db0ca48abfa2adb4004c",
+    "url": "/star-wars-planet-search/index.html"
+  },
+  {
+    "revision": "7c515cac6ae7f81d9ad1",
+    "url": "/star-wars-planet-search/static/css/main.0c8dd27f.chunk.css"
+  },
+  {
+    "revision": "971f84054aa607b2a6ed",
+    "url": "/star-wars-planet-search/static/js/2.accf6ddb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/star-wars-planet-search/static/js/2.accf6ddb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c515cac6ae7f81d9ad1",
+    "url": "/star-wars-planet-search/static/js/main.25f4a466.chunk.js"
+  },
+  {
+    "revision": "430eadddf8313e3d766d",
+    "url": "/star-wars-planet-search/static/js/runtime-main.2983b150.js"
+  }
+]);
